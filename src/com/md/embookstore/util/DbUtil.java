05269f25f5de2f5ee91f64be7004b74d7be6b59d
Mohammadhussain257 <Mohammadhusen257@gmail.com>
2018-10-19 16:17:06 +0545

    updating site

--- src/com/md/embookstore/util/DbUtil.java
+++ src/com/md/embookstore/util/DbUtil.java
@@ -9,7 +9,7 @@ public class DbUtil {
 	private static final String DB_NAME = "embookstore";
 	private static final String URL = "jdbc:mysql://localhost:3306/";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "998849121";
+	private static final String PASSWORD = "root";
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName(DRIVER_NAME);
 		return DriverManager.getConnection(URL + DB_NAME, USERNAME, PASSWORD);
